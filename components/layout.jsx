f9516f825cd4195098ac866fb806d2719bd6cbe3
Nate <nate@nate-web3.com>
2022-04-20 19:38:28 +0300

    Update layout.jsx

--- components/layout.jsx
+++ components/layout.jsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import { Box } from '@chakra-ui/react';
 
 import Footer from './Footer';
-import Navbar from './Navbar';
+import Navbar from './navbar';
 
 export default function Layout({ children }) {
   return (
