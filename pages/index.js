b0d1a199bf2b19201c65f1376fb65a0b4aba524c
Nate <nate@nate-web3.com>
2022-04-21 15:57:03 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
-import Property from '../components/Property';
+import Property from '../components/property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
